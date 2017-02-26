20313d549820d17fa9404469b10eab5214b69f4f
暑假项目 <1041874421@qq.com>
2017-02-26 16:55:51 +0800

    resume 属性同时支持数组和对象

--- resumer/src/components/ResumeEditor.vue
+++ resumer/src/components/ResumeEditor.vue
@@ -3,13 +3,27 @@
         <nav>
             <ol>
                 <li v-for="(item,index) in resume.config" :class="{active: item.field === selected}" @click="selected = item.field">
-                    {{index}}
+                   <svg class="icon">
+                        <use :xlink:href="`#icon-${item.icon}`"></use>
+                   </svg>
                 </li>
             </ol>
         </nav>
         <ol class="panels">
             <li v-for="item in resume.config" v-show="item.field === selected">
-                {{resume[item.field]}}
+                <div v-if="resume[item.field] instanceof Array">
+                    <div class="subitem" v-for="subitem in resume[item.field]">
+                        <div class="resumeField" v-for="(value,key) in subitem">
+                            <label >{{key}}</label>
+                            <input type="text" :value="value">
+                        </div>
+                        <hr>
+                    </div>
+                </div>
+                <div v-else class="resumeField" v-for="(value,key) in resume[item.field]">
+                    <label> {{key}}</label>
+                    <input type="text" v-model="resume[item.field][key]">
+                </div>
             </li>
         </ol>
     </div>
@@ -35,17 +49,32 @@
                         city:'',
                         title:''
                     },
-                    'work history':[],
-                    education:[],
-                    projects:[],
-                    awards:[],
-                    contacts:[],
+                    'work history':[
+                        {company: 'AL', content: '我的第二份工作是'},
+                        {company: 'TX', content: '我的第一份工作是'},
+                    ],
+                    education:[
+                        {school: 'AL', content: '文字'},
+                        {school: 'TX', content: '文字'},
+                    ],
+                    projects:[
+                        {name: 'project A', content: '文字'},
+                        {name: 'project B', content: '文字'},
+                    ],
+                    awards:[
+                        {name: 'award A', content: '文字'},
+                        {name: 'award B', content: '文字'},
+                    ],
+                    contacts:[
+                        {contaxt: 'phone', content: '187657234'},
+                        {contaxt: 'qq', content: '12345678'},
+                    ],
                 }
             }
         }
     }
 </script>
-<style lang="less">
+<style lang="less" scoped>
     #resumeEditor{
         background:#fff;
         box-shadow: 0 1px 3px 0 rgba(0,0,0,0.25);
@@ -71,8 +100,36 @@
                 }
             }
         }
+        > .panels{
+            flex-grow: 1;
+            > li{
+                padding:24px;
+            }
+        }
+        svg.icon{
+            width:24px;
+            height:24px;
+        }
         ol{
             list-style: none;
+        }
+        .resumeField{
+            >label{
+                display:block;
+            }
+            input[type=text]{
+                margin:16px 0;
+                border:1px solid #ddd;
+                box-shadow:inset 0 1px 3px 0 rgba(0,0,0,0.25);
+                width:100%;
+                height:40px;
+                padding:0 8px;
+            }
+        }
+        hr{
+            border:none;
+            border-top:1px solid #ddd;
+            margin:24px 0;
         }
 
     }
