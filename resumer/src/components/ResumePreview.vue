2044a074ab6f4990f12722f49ca93b5f897cf079
暑假项目 <1041874421@qq.com>
2017-02-26 14:32:53 +0800

    add three components

---
+++ resumer/src/components/ResumePreview.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="resumePreview">
+        I am resumePreview
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ResumePreview'
+    }
+</script>
+<style>
+    #resumePreview{
+        color:blue;
+    }
+</style>
