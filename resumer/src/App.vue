74e2f2696fcd301b33210bc324e70753859db46c
暑假项目 <1041874421@qq.com>
2017-02-26 13:20:12 +0800

    copy-run-modify

---
+++ resumer/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+      <p>{{text}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data: function () {
+    return {
+      text: '你好'
+    }
+  }
+}
+</script>
+
+<style>
+ p{color:red;}
+</style>
