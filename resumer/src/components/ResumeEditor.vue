b4080a0d2822667a830872ace9f0c57c14c03e4b
Kevin <1041874421@qq.com>
2017-03-08 21:34:55 +0800

    调整数据： 1.将resumeEditor的数据转移到store

--- resumer/src/components/ResumeEditor.vue
+++ resumer/src/components/ResumeEditor.vue
@@ -36,49 +36,20 @@
 <script>
     export default {
         name: 'ResumeEditor',
-        data(){
-            return {
-                selected:'profile',
-                resume:{
-                    config:[
-                        {field: "profile",icon: "id"},
-                        {field: "work history",icon: "work"},
-                        {field: "education",icon: "book"},
-                        {field: "projects",icon: "heart"},
-                        {field: "awards",icon: "cup"},
-                        {field: "contacts",icon: "phone"},
-                    ],
-                    profile:{
-                        name:'baixiaoji',
-                        city:'Hangzhou',
-                        title:'blablabla'
-                    },
-                    'work history':[
-                        {company: 'XIN', content: '我的第二份工作是'},
-                        {company: 'FF', content: '我的第一份工作是'},
-                    ],
-                    education:[
-                        {school: 'SD', content: '文字'},
-                        {school: 'TX', content: '文字'},
-                    ],
-                    projects:[
-                        {name: 'project A', content: '文字'},
-                        {name: 'project B', content: '文字'},
-                    ],
-                    awards:[
-                        {name: 'award A', content: '文字'},
-                        {name: 'award B', content: '文字'},
-                    ],
-                    contacts:[
-                        {contaxt: 'phone', content: '187657234'},
-                        {contaxt: 'qq', content: '12345678'},
-                    ],
-                }
-            }
-        },
         computed: {
             count () {
                 return this.$store.state.count
+            },
+            selected :{
+                get(){
+                    return this.$store.state.selected
+                },
+                set(value){
+                    return this.$store.commit('switchTab',value)
+                }
+            },
+            resume (){
+                return this.$store.state.resume
             }
         },
         methods:{
--- resumer/src/store/index.js
+++ resumer/src/store/index.js
@@ -4,11 +4,49 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
     state:{
-        count: 0
+        count: 0,
+        selected:'profile',
+        resume:{
+            config:[
+                {field: "profile",icon: "id"},
+                {field: "work history",icon: "work"},
+                {field: "education",icon: "book"},
+                {field: "projects",icon: "heart"},
+                {field: "awards",icon: "cup"},
+                {field: "contacts",icon: "phone"},
+            ],
+            profile:{
+                name:'baixiaoji',
+                city:'Hangzhou',
+                title:'blablabla'
+            },
+            'work history':[
+                {company: 'XIN', content: '我的第二份工作是'},
+                {company: 'FF', content: '我的第一份工作是'},
+            ],
+            education:[
+                {school: 'SD', content: '文字'},
+                {school: 'TX', content: '文字'},
+            ],
+            projects:[
+                {name: 'project A', content: '文字'},
+                {name: 'project B', content: '文字'},
+            ],
+            awards:[
+                {name: 'award A', content: '文字'},
+                {name: 'award B', content: '文字'},
+            ],
+            contacts:[
+                {contaxt: 'phone', content: '187657234'},
+                {contaxt: 'qq', content: '12345678'},
+            ],
+        }
     },
+    
     mutations:{
         increment(state){
             state.count++
-        }
+        },
+        
     }
 })
