617d4b1c1a4cb9137f7b895707467cd73c86d764
Kevin <1041874421@qq.com>
2017-03-19 13:57:48 +0800

    调整登录代码顺序

--- resumer/src/components/Topbar.vue
+++ resumer/src/components/Topbar.vue
@@ -9,20 +9,20 @@
                 </div>
                 <div v-else class="userActions">
                     <a href="#" @click.prevent="signUpDialogVisible=true" class="button primary">注册</a>
-                    <MyDialog title="注册" :visible="signUpDialogVisible" @close="signUpDialogVisible= false">
-                        <SignUpForm @success="signIn($event)"/>
-                    </MyDialog>                    
+
                     <a href="#" class="button" @click.prevent="signInDialogVisible = true">登录</a>
-                    <MyDialog titel="登录" :visible="signInDialogVisible" @close="signInDialogVisible = false">
-                        <SignInForm />
-                    </MyDialog>
+
                 </div>
                 <button class="button primary">保存</button>
                 <button class="button">预览</button>
             </div>
         </div>
-
-
+        <MyDialog title="注册" :visible="signUpDialogVisible" @close="signUpDialogVisible= false">
+            <SignUpForm @success="signIn($event)" />
+        </MyDialog>
+        <MyDialog titel="登录" :visible="signInDialogVisible" @close="signInDialogVisible = false">
+            <SignInForm />
+        </MyDialog>
     </div>
 </template>
 
@@ -33,84 +33,85 @@
     import AV from "../lib/leancloud"
     export default {
         name: 'Topbar',
-        data(){
+        data() {
             return {
                 signUpDialogVisible: false,
                 signInDialogVisible: false
             }
         },
-        components:{
-            MyDialog, SignUpForm,SignInForm
+        components: {
+            MyDialog, SignUpForm, SignInForm
         },
         computed: {
-            user(){
+            user() {
                 return this.$store.state.user
             },
-            logined(){
+            logined() {
                 return this.user.id
             }
         },
         methods: {
-            signOut(){
+            signOut() {
                 AV.User.logOut()
                 this.$store.commit("removeUser")
             },
-            signIn(user){
+            signIn(user) {
                 this.signUpDialogVisible = false
-                this.$store.commit("setUser",user)
+                this.$store.commit("setUser", user)
             }
         }
     }
+
 </script>
 <style scoped lang="less">
-    #topbar{
-        background:#fff;
-        box-shadow:0 1px 3px 0 rgba(0,0,0,0.25);
-        
-        >.wrapper{
-            min-width:1024px;
-            max-width:1440px;
-            margin:0 auto;
-            height:64px;
+    #topbar {
+        background: #fff;
+        box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.25);
+        >.wrapper {
+            min-width: 1024px;
+            max-width: 1440px;
+            margin: 0 auto;
+            height: 64px;
         }
-        >.wrapper{
+        >.wrapper {
             display: flex;
             justify-content: space-between;
             align-items: center;
-            padding:0 16px;
+            padding: 0 16px;
         }
-        .logo{
+        .logo {
             font-size: 24px;
-            color:#000;
+            color: #000;
         }
     }
-    .button{
-        width:72px;
-        height:32px;
-        border:none;
-        cursor:pointer;
+    
+    .button {
+        width: 72px;
+        height: 32px;
+        border: none;
+        cursor: pointer;
         font-size: 18px;
         background: #ddd;
-        color:#222;
-        text-decoration:none;
+        color: #222;
+        text-decoration: none;
         display: inline-flex;
         justify-content: center;
         align-items: center;
         vertical-align: middle;
-        &:hover{
-            box-shadow:1px 1px 1px hsla(0,0,0,0.50);
+        &:hover {
+            box-shadow: 1px 1px 1px hsla(0, 0, 0, 0.50);
         }
-        &.primary{
+        &.primary {
             background: #02af5f;
-            color:white;
+            color: white;
         }
     }
-
+    
     .actions {
         display: flex;
-        .userActions{
+        .userActions {
             margin-right: 3em;
-            .welcome{
+            .welcome {
                 margin-right: .5em;
             }
         }
