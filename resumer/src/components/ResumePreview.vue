b918b5044f2a35f806ec8e306b92b80c73bb3bc6
Kevin <1041874421@qq.com>
2017-03-24 14:00:32 +0800

    修改数据结构 1.改完store数据结构 2.Editor 判断也要改回来 3. Previw也要修改显示数据

--- resumer/src/components/ResumePreview.vue
+++ resumer/src/components/ResumePreview.vue
@@ -10,7 +10,7 @@
             <small>{{resume.profile.birthday}}</small>
         </p>
         </section>
-        <section data-name='projects' v-show="resume.projects">
+        <section data-name='projects' v-show="resume.projects.length > 0">
             <h2>项目经历</h2>
             <ol>
                 <li v-for="item in resume.projects">
@@ -19,7 +19,7 @@
                 </li>
             </ol>
         </section>
-        <section data-name="workHistory" v-show="resume.workHistory">
+        <section data-name="workHistory" v-show="resume.workHistory.length > 0">
         <h2>工作经历</h2>
         <ol>
             <li v-for="item in resume.workHistory">
@@ -29,7 +29,7 @@
         </ol>
         </section>
 
-        <section data-name="education" v-show="resume.education">
+        <section data-name="education" v-show="resume.education.length > 0">
         <h2>毕业院校</h2>
         <ol>
             <li v-for="item in resume.education">
@@ -39,7 +39,7 @@
             </li>
         </ol>
         </section>
-        <section data-name='awards' v-show="resume.awards">
+        <section data-name='awards' v-show="resume.awards.length > 0">
             <h2>获奖情况</h2>
             <ol>
                 <li v-for="item in resume.awards">
@@ -48,7 +48,7 @@
                 </li>
             </ol>
         </section>
-        <section data-name='contacts' v-show="resume.contacts">
+        <section data-name='contacts' v-show="resume.contacts.length > 0">
             <h2>联系方式</h2>
             <table>
                 <tr v-for="item in resume.contacts">
